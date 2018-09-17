1fd616f403d65d89ca20b31e8d0795baa4f3baf3
Cedric David <cedric.david@btopenworld.com>
2018-09-17 02:42:18 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
-
-  create_table "students", force: :cascade do |t|
-    t.string "name"
-    t.integer "grade"
-    t.datetime "birthdate"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
